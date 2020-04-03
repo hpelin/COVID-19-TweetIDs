790589dc75a0fcb814d5168ad02d40ef80e94fed
lucas_miranda <lucasmiranda42@gmail.com>
2020-04-03 21:19:01 +0200

    cleanup

--- sample_ids.py
+++ sample_ids.py
@@ -25,11 +25,11 @@ parser.add_argument(
     default=".",
 )
 parser.add_argument(
-    "-n",
-    "--sample_size",
+    "-f",
+    "--fraction_size",
     type=int,
-    help="Sample size per file. Int, default is 10000",
-    default=10000,
+    help="1/fraction of length of file to keep. Int, default is 1000",
+    default=1000,
 )
 
 args = parser.parse_args()
@@ -37,17 +37,24 @@ np.random.seed(args.random_seed)
 
 
 tweet_IDs = [tweets for tweets in os.listdir(args.path) if tweets.endswith("txt")]
+Sampled_tweet_IDs = np.array([])
 
 for tweet_file in tqdm(tweet_IDs):
     with open(args.path + tweet_file, "r") as t:
         tweets = np.array([line.replace("\n", "") for line in t])
 
-    tweets = np.random.choice(tweets, args.sample_size)
+    Sampled_tweet_IDs = np.append(
+        Sampled_tweet_IDs, np.random.choice(tweets, len(tweets) // args.sample_size)
+    )
 
-    with open(
-        "_".join([args.path + "Random", str(args.sample_size), tweet_file]), "w"
-    ) as x:
-        for tweetID in tweets:
-            x.write(tweetID + "\n")
+with open(
+    "_".join(
+        [args.path + "Random", str(args.sample_size), args.path.replace("/", "-"),]
+    )
+    + ".txt",
+    "w",
+) as x:
+    for tweetID in Sampled_tweet_IDs:
+        x.write(tweetID + "\n")
 
 print("Done!")
